2e5a00cad3b9f6e583c3baad485e194e2e1429d5
Filip Cima <filip.cima@firma.seznam.cz>
2020-10-22 17:24:43 +0200

    added mapping to test_1 preparation

--- test_1.py
+++ test_1.py
@@ -1,3 +1,72 @@
+from collections import defaultdict
+
+# Mappings
+def mapping(R, A):
+    set_a = [a for (a, b) in R]
+    
+    for a in A:
+        if a not in set_a:
+            return False
+
+    occurances_a = defaultdict(lambda: 0)
+    for item in set_a:
+        occurances_a[item] += 1
+
+    for i in occurances_a.values():
+        if i > 1 or i == 0:
+            return False
+
+    return True
+
+
+def injection(R, A):
+    if not mapping(R, A):
+        raise Exception(f"Relation R={R} on set {A} isn't mapping.")
+
+    set_b = [b for (a, b) in R]
+
+    occurances_b = defaultdict(lambda: 0)
+    for item in set_b:
+        occurances_b[item] += 1
+
+    return all([i == 0 or i == 1 for i in occurances_b.values()])
+
+
+def surjection(R, A):
+    if not mapping(R, A):
+        raise Exception(f"Relation R={R} on set {A} isn't mapping.")
+
+    set_b = [b for (a, b) in R]
+
+    occurances_b = defaultdict(lambda: 0)
+    for i in A:
+        occurances_b[i] = 0
+
+    for item in set_b:
+        occurances_b[item] += 1
+
+    return all([i >= 1 for i in occurances_b.values()])
+
+
+def bijection(R, A):
+    if not mapping(R, A):
+        raise Exception(f"Relation R={R} on set {A} isn't mapping.")
+
+    set_a = [b for (a, b) in R]
+    set_b = [b for (a, b) in R]
+
+    if not len(set_a) == len(set_b):
+        return False
+    
+    inj = injection(R, A)
+    sur = surjection(R, A)
+
+    if inj and sur:
+        return True
+
+    return False
+
+# Relations
 def reflexive(R, A):
     """Returns True if a relation R on set A is reflexive, False otherwise."""
     for a in A:
@@ -62,6 +131,7 @@ def transitive(R, A):
     
     return True
 
+# Relace
 
 # Pokud nemas na pocitaci python, pouzij https://repl.it/languages/python3
 # A = Mnozina, do ktere patri relace R
@@ -78,3 +148,15 @@ print(f"Is symmetric: {symmetric(R, A)}")
 print(f"Is asymmetric: {asymmetric(R, A)}")
 print(f"Is antisymmetric: {antisymmetric(R, A)}")
 print(f"Is transitive: {transitive(R, A)}")
+
+
+# Zobrazeni
+A1 = [1, 2, 3]
+R1 = [(1, 2), (2, 3), (3, 1)]
+
+print(f"\n\nProperties for mapping R1={R} on set A1={A}\n")
+
+print(f"Is mapping: {mapping(R1, A1)}")
+print(f"Is injection: {injection(R1, A1)}")
+print(f"Is surjection: {surjection(R1, A1)}")
+print(f"Is bijection: {bijection(R1, A1)}")
